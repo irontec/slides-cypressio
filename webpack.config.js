f803ab7695f4168a06bd2cda8b87e6a346890dbe
Javier Infante <jabi@irontec.com>
2018-10-03 06:57:51 +0200

    updated to babel 7. tree-shaking

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require("webpack"),
   path = require("path"),
   HtmlWebpackPlugin = require("html-webpack-plugin"),
   CopyWebpackPlugin = require("copy-webpack-plugin"),
+  BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin,
   MiniCssExtractPlugin = require("mini-css-extract-plugin"),
   ImageminPlugin = require("imagemin-webpack-plugin").default;
 
@@ -26,6 +27,7 @@ module.exports = {
     contentBase: path.resolve(__dirname, "dist")
   },
   plugins: [
+    // new BundleAnalyzerPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
     new CopyWebpackPlugin([{
       from: path.resolve(__dirname, "content", "media"),
@@ -84,9 +86,34 @@ module.exports = {
           {
             loader: "babel-loader",
             options: {
-              plugins: [],
-              presets: ["env", "stage-0"]
-            }
+              presets: [
+                /*
+                  To get tree shaking working, we need the `modules: false` below.
+                  https://goo.gl/4vZBSr - 2ality blog mentions that the issue is caused
+                  by under-the-hood usage of `transform-es2015-modules-commonjs`.
+                  https://goo.gl/sBmiwZ - A comment on the above post shows that we
+                  can use `modules: false`.
+                  https://goo.gl/aAxYAq - `babel-preset-env` documentation.
+                */
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: {
+                      browsers: [
+                        'last 2 versions'
+                      ]
+                    },
+                    modules: false // Needed for tree shaking to work.
+                  }
+                ]
+              ],
+      
+              // https://goo.gl/N9gaqc - List of Babel plugins.
+              plugins: [
+                '@babel/plugin-proposal-object-rest-spread', // https://goo.gl/LCHWnP
+                '@babel/plugin-proposal-class-properties' // https://goo.gl/TE6TyG
+              ]
+      }
           },
           {loader: 'condition-loader', options: optsIfDef } 
         ]
